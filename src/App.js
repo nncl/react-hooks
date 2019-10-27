cfa1da134cf3fb54ecd3e03110e329494c263af1
= <nncl@live.com>
2019-10-27 00:17:33 -0300

    Creating states with useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <h1>Hello World</h1>;
+  const [tech, setTech] = useState(['React JS', 'React Native']);
+  const [newTech, setNewTech] = useState('');
+
+  function handleAdd() {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }
+
+  return (
+    <>
+      <ul>
+        {tech.map(item => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+
+      <input
+        type="text"
+        value={newTech}
+        onChange={e => setNewTech(e.target.value)}
+      />
+
+      <button type="button" onClick={handleAdd}>
+        Add
+      </button>
+    </>
+  );
 }
 
 export default App;
